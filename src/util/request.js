6bde787e699061126b7049f2a811b8d9adb3a158
AloneBlackCat <2210439454@qq.com>
2023-12-18 17:18:46 +0800

    封装请求方法

---
+++ src/util/request.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+// 记录公共的前缀
+const baseURL = "http://localhost:81";
+const instance = axios.create({baseURL});
+
+// 添加响应拦截器
+instance.interceptors.response.use(
+    result=>{
+        return result.data;
+    },
+    error => {
+        alert("服务异常");
+        // 异步状态转换为失败的状态
+        return Promise.reject(error);
+    }
+)
+
+export default instance;
