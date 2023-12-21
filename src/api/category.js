138fc2ea22f67310a04b50d1cc1caa422af75506
AloneBlackCat <2210439454@qq.com>
2023-12-21 16:06:15 +0800

    axios请求拦截器中添加全局token

--- src/api/category.js
+++ src/api/category.js
@@ -1,12 +1,6 @@
 import request from '@/utils/request.js'
-import { useTokenStore } from '@/stores/token.js'
 
 export const categoryListService = () => {
     // 获取token状态
-    const tokenStore = useTokenStore()
-    return request.get('/category', {
-        headers: {
-            'Authorization' : tokenStore.token
-        }
-    })
+    return request.get('/category')
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,19 +1,33 @@
 import axios from "axios"
 import {ElMessage} from "element-plus";
+import { useTokenStore } from "@/stores/token.js";
 // 记录公共的前缀
 const baseURL = "/api";
 const instance = axios.create({baseURL});
 
+// 添加请求拦截器
+instance.interceptors.request.use(
+    (config) => {
+        let tokenStore = useTokenStore()
+        if (tokenStore.token) {
+            config.headers.Authorization = tokenStore.token
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
 // 添加响应拦截器
 instance.interceptors.response.use(
-    result => {
+    (result) => {
         if (result.data.code === 0) {
             return result.data
         }
         ElMessage.error({message: result.data.message ? result.data.message : '服务异常'});
         return Promise.reject(result.data)
     },
-    error => {
+    (error) => {
         ElMessage.error({message: "服务异常"});
         // 异步状态转换为失败的状态
         return Promise.reject(error);
